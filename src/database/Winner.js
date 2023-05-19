6dcdb3bf362e6a827409955cbd276a7716a4dd7a
Eduardo Mendoza <eduardojmendoza@gmail.com>
2023-05-19 16:11:37 -0500

    Winner updated

--- src/database/Winner.js
+++ src/database/Winner.js
@@ -33,8 +33,6 @@ const getOneWinner = async (winnerId) => {};
 
 const createNewWinner = async (newWinner) => {
   try {
-    const winner = await Winner.create(newWinner);
-    return winner;
   } catch (error) {
     throw { status: 500, message: error?.message || error };
   }
@@ -53,80 +51,5 @@ const deleteOneWinner = (winnerId) => {
     throw { status: error?.status || 500, message: error?.message || error };
   }
 };
-
-// const getAllWinners = (filterParams) => {
-//   return new Promise((resolve, reject) => {
-//     let query = "SELECT * FROM winners";
-//     if (filterParams.name) {
-//       query += " where nombre='" + filterParams.name + "'";
-//     }
-//     console.log(query);
-//     connection.query(query, (error, results) => {
-//       if (error) reject({ status: 500, message: error });
-//       resolve(Object.values(JSON.parse(JSON.stringify(results))));
-//     });
-//   });
-// };
-
-// const getOneWinner = (winnerId) => {
-//   try {
-//     return new Promise((resolve, reject) => {
-//       connection.query("SELECT * FROM winners where id='" + winnerId + "'", (error, results) => {
-//         if (error) reject({ status: 500, message: error });
-//         if (Object.values(JSON.parse(JSON.stringify(results))).length === 0) {
-//           reject({
-//             status: 400,
-//             message: `Can't find winner with the id '${winnerId}'`,
-//           });
-//         }
-//         resolve(Object.values(JSON.parse(JSON.stringify(results))));
-//       });
-//     });
-//   } catch (error) {
-//     throw { status: error?.status || 500, message: error?.message || error };
-//   }
-// };
-
-// const createNewWinner = (newWinner) => {
-//   try {
-//     return new Promise((resolve, reject) => {
-//       connection.query("INSERT INTO winners(nombre, fecha, actualizado) VALUES (?,?,?) ", [newWinner.nombre, newWinner.fecha, newWinner.actualizado], (error, results) => {
-//         if (error) reject(error);
-//         resolve(JSON.parse(JSON.stringify(results)));
-//       });
-//     });
-//   } catch (error) {
-//     throw { status: 500, message: error?.message || error };
-//   }
-// };
-
-// const updateOneWinner = (winnerId, changes) => {
-//   try {
-//     return new Promise((resolve, reject) => {
-//       let query = `UPDATE winners SET nombre="${changes.nombre}", fecha="${changes.fecha}", actualizado="${changes.actualizado}" WHERE id="${winnerId}"`;
-//       connection.query(query, (error, results) => {
-//         if (error) {
-//           reject(error.sqlMessage);
-//         }
-//         resolve(JSON.parse(JSON.stringify(results)));
-//       });
-//     });
-//   } catch (error) {
-//     throw { status: error?.status || 500, message: error?.message || error };
-//   }
-// };
-
-// const deleteOneWinner = (winnerId) => {
-//   try {
-//     return new Promise((resolve, reject) => {
-//       connection.query("Delete from winners where id = ?", [winnerId], (error, results) => {
-//         if (error) reject(error);
-//         resolve(JSON.parse(JSON.stringify(results)));
-//       });
-//     });
-//   } catch (error) {
-//     throw { status: error?.status || 500, message: error?.message || error };
-//   }
-// };
 
 module.exports = { getAllWinners, createNewWinner, getOneWinner, deleteOneWinner, updateOneWinner };
